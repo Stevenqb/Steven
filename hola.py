d66e638681f3e445fce7001f2c514cf3985bb171
Stevenqb <stevencastro089@gmail.com>
2025-04-15 13:19:21 -0400

    python primera clase

--- hola.py
+++ hola.py
@@ -9,6 +9,43 @@
 # print(n1+n2)
 
 
-n1=int(input ("ingrese un numero "))
-n2=int(input ("ingrese un numero "))
-print("el resultado de la suma es" , n1+n2)
+#sacar promedio 3 numeros 
+# print("ingrese 3 numeros para sacar su promedio")
+# n1=int(input("ingrese un numero "))
+# n2=int(input("ingrese un numero "))
+# n3=int(input("ingrese un numero "))
+# prom=(n1+n2+n3)/3
+# print("el promedio es", prom)
+
+# print("ingrese su edad")
+# edad=int(input("ingrese su edad"))
+
+# if edad>=18:
+#     print("usted es mayor de edad")
+# else:
+#     print("usted es menor de edad")
+
+# edad=int (input("ingrese su edad: "))
+
+# if edad<12:
+#     print("tiene",edad,"por tanto es un niño")
+# elif edad>= 12 and edad<=17:
+#     print("tiene", edad,"por tanto es un adolescente")
+# else:
+#     print("tiene", edad, "por tanto es mayor de edad")
+
+
+n1=int(input("ingrese un numero"))
+n2=int(input("ingrese otro numero"))
+n3=int(input("ingrese otro numero"))
+
+if n1>n2 and n1>n3:
+    print("el numero", n1, "es el mayor")
+if n2>n3:
+    print("el numero", n2, "es el mayor")
+else:
+    print ("el numero", n3, "es el mayor")
+    
+
+
+
